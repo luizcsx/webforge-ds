0aae44ce3cb0619d478e30005162074922e099bb
Luiz Miguel <contato.luiz404@gmail.com>
2026-05-25 17:15:46 -0300

    feat: integrate VFS and parser dispatch flow into main boot sequence

--- source/main.cpp
+++ source/main.cpp
@@ -1,40 +1,33 @@
 #include <nds.h>
-#include <fat.h>
 #include <stdio.h>
+#include "vfs.h"
+#include "parser.h"
 
-void systemCoreInit() {
-    bool storageReady = fatInitDefault();
-
+int main(void) {
     videoSetMode(MODE_0_2D);
     vramSetBankA(VRAM_A_MAIN_BG);
     consoleInit(NULL, 1, BgType_Text4bpp, BgSize_T_256x256, 0, 1, true, true);
 
-    printf("================================\n");
-    printf("           WebForge DS          \n");
-    printf("================================\n");
-    
-    if (storageReady) {
-        printf("[SYS] VFS File System: LINKED.\n");
+    printf("=== WebForge DS ===\n");
+
+    if (!vfsInit()) {
+        printf("[ERR] VFS: SD init failed.\n");
     } else {
-        printf("[ERR] VFS Storage IO: FAILED.\n");
-    }
-    
-    printf("Kernel State: Ready.\n\n");
-}
+        printf("[SYS] VFS: Ready.\n");
 
-int main(void) {
-  systemCoreInit();
-
-    while(1) {
-        scanKeys();
-        uint32 keys = keysDown();
-        
-        if (keys & KEY_START) {
-            break;
+        VFSBuffer buf = vfsLoad("nitro:/index.per");
+        if (buf.valid) {
+            printf("[VFS] Loaded %d bytes [%d]\n", buf.size, buf.type);
+            ParseResult res = parserDispatch(&buf);
+            printf("[PAR] %s\n", res.success ? "OK" : res.errorMsg);
+            vfsFree(&buf);
         }
+    }
 
+    while (1) {
+        scanKeys();
+        if (keysDown() & KEY_START) break;
         swiWaitForVBlank();
     }
-
     return 0;
 }
